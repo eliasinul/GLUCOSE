cc88509ea0e487543d1bd3298d7e2eb3ca5f671e
AgnesBelt <beltramo@kth.se>
2024-09-12 14:46:04 +0200

    expanding on the bash file to automate the deploy of the GLUCOSE-customised workflows to: 1) run GLUCOSE scenarios and process results; 2) run scenario discovery with GLUCOSE for RDM analysis using either Sobol or LHS methos (ref to glucose_esom_gsa GitHub repo).

--- deploy_GLUCOSE.sh
+++ deploy_GLUCOSE.sh
@@ -1,28 +1,79 @@
 #!/bin/bash
 # Load the glpk mode which includes glpsol, please check version this command is specific for Tegner
 # Set the path to glpk (need to change) if not on Tegner
-module add glpk/4.65
+# module add glpk/4.65 only needed for kth cluster
 
+cd .. || exit 
+cd .. || exit 
 
-# Your Git repository URL 
-repo_url="https://github.com/AgnesBelt/GLUCOSE.git" 
+## Provide the terminal command to install glpk
+echo Install glpk via Homebrew - for Mac-users only
+# brew install glpk || exit
+
+## GLUCOSE Git repository URL 
+# repo_url="https://github.com/AgnesBelt/GLUCOSE.git" 
  
 # Directory to clone the repository into 
-clone_dir="repo_clone" 
+GLUCOSE_dir="KTH-dESA/GLUCOSE/" 
 
-# Clone the Git repository 
-git clone "$repo_url" "$clone_dir" 
+## Clone the Git repository 
+# echo Clone GitHUb repository: AgnesBelt/GLUCOSE || exit
+# git clone "$repo_url" 
 
-# Navigate into the cloned repository directory 
-cd "$clone_dir" || exit 
+## Navigate into the cloned repository directory 
+echo Navigate to the folder: KTH-dESA/GLUCOSE/
+cd "$GLUCOSE_dir" || exit 
 
-# Execute the snakemake workflow for GLUCOSE:
+## Install and create conda environment: snakemake
+# echo Install and create conda environment : snakemake
+# conda install -c conda-forge mamba || exit
+# mamba create -c bioconda -c conda-forge -n snakemake snakemake-minimal pandas || exit
+
+## Activate conda environment snakemake
+echo Activate conda environment: snakemake
+source /Users/agnesebeltramo/anaconda3/etc/profile.d/conda.sh || exit 
+conda activate snakemake || exit 
+
+## Execute the snakemake workflow for GLUCOSE:
     # 1. read input_data in .csv
     # 2. generate .lp, run the model with GUROBI
     # 3. generate solution .sol file
     # 4. convert .sol file into .csv results files
     # 5. process results and generate results visualisation
 
-snakemake --cores 4 --use-conda
+# git branch GLUCOSE_rdm || exit
 
+echo Execute workflow to run and process GLUCOSE results
+snakemake --cores 4 --use-conda || exit 
+
+cd .. || exit 
+cd .. || exit 
+
+## Adapted esom_sga workflow for RDM runs: Git repository URL 
+# repo_url="https://github.com/AgnesBelt/glucose_esom_gsa.git" 
+
+# Directory to clone the repository into 
+GLUCOSE_rdm_dir="AgnesBelt/glucose_esom_gsa/"
+
+## Navigate into the cloned repository directory 
+echo Navigate to the folder: AgnesBeltr/glucose_esom_gsa/
+cd "$GLUCOSE_dir" || exit 
+# git branch envs_glucose || exit
+
+## Execute the adapted snakemake workflow for RDM based on esom_gsa:
+# pip install -r requirements.txt ||exit
+
+## Copy the GLUCOSE scenario of reference to the glucose_esom_gsa/congif folder
+echo Copy the GLUCOSE scenario of reference to the glucose_esom_gsa/congif folder
+# cd input_data
+# cp -r /Users/agnesebeltramo/Documents/GitHub_repo/KTH-dESA/GLUCOSE/input_data/GLUCOSE_noDA2CS_14_9/.   /Users/agnesebeltramo/Documents/GitHub_repo/AgnesBelt/glucose_esom_gsa/congif/model_input/GLUCOSE_data_csv/
+
+SOURCE="/input_data"
+DESTINATION="/Users/agnesebeltramo/Documents/GitHub_repo/AgnesBelt/glucose_esom_gsa/congif"
+
+cp -r "$SOURCE/GLUCOSE_noDA2CS_14_9/"*  "$DESTINATION/model_input/"
+
+## Execute the glucose_esom_gsa workflow
+echo Execute the glucose_esom_gsa workflow 
+snakemake --use-conda --cores 4 --resources mem_mb=16000 disk_mb=30000 || exit
 
